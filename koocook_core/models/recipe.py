02e52c1589d84f07484147619eabd6c2ec01909e
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-10-27 16:31:29 +0700

    Change Recipe.author to use RecipeAuthor

--- koocook_core/models/recipe.py
+++ koocook_core/models/recipe.py
@@ -3,13 +3,14 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 from koocook_core.models.nutrition import NutritionInfo, Ingredient
+from koocook_core.models.user import RecipeAuthor
 
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
     image = models.URLField()
 
-    author = models.ManyToManyField(User)
+    author = models.ForeignKey(RecipeAuthor, on_delete=models.PROTECT)
     date_published = models.DateTimeField()
     description = models.CharField(max_length=255)
     prep_time = models.DurationField()
