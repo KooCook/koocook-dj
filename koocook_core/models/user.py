c7016d2c45c29487c23f4ec0aeb5ec66253a6b5b
T. Bunratta <tharathorn.bu@ku.th>
2019-10-27 16:22:15 +0700

    Rename model Author to RecipeAuthor

--- koocook_core/models/user.py
+++ koocook_core/models/user.py
@@ -2,5 +2,6 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Author(models.Model):
+class RecipeAuthor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
