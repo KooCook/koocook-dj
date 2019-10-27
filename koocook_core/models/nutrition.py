86e86d5dbf6ccaf72af17e403359b1ac3707df83
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-10-27 17:30:26 +0700

    Rename NutritionInfo to Nutrition

--- koocook_core/models/nutrition.py
+++ koocook_core/models/nutrition.py
@@ -3,9 +3,9 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
-class NutritionInfo(models.Model):
+class Nutrition(models.Model):
     calories = models.IntegerField(validators=[MinValueValidator(0)])
 
 
 class Ingredient(models.Model):
-    pass
+    nutrition = models.OneToOneField(Nutrition, on_delete=models.CASCADE)
