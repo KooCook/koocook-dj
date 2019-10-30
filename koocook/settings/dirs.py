56e2a2234a72fb39f09b87904dc34f8e202ad9b0
T. Bunratta <tharathorn.bu@ku.th>
2019-10-30 11:56:48 +0700

    Remove the dependency 'Unipath'

--- koocook/settings/dirs.py
+++ koocook/settings/dirs.py
@@ -1,3 +1,4 @@
 from unipath import Path
+import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).parent  # os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
