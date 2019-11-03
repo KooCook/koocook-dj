6dbf310d1eb3d505351da972f024dc8b227d9fed
T. Bunratta <tharathorn.bu@ku.th>
2019-11-03 13:38:41 +0700

    Separate columns of user recipes in half

--- koocook_core/views/__init__.py
+++ koocook_core/views/__init__.py
@@ -11,8 +11,8 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import UpdateView
 
+
 @require_http_methods(["GET"])
-# Create your views here.
 def index(request):
     return render(request, 'index.html')
 
