dc6beb7a0f3dd6c42a32d73b290c1541dd572af2
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-11-03 07:19:22 +0700

    Refactor + Add "programmatic access" to `Unit`s
    "Programmatic access" now supports both singular and plural forms of the
    unit, its symbol, and with or without degrees for temperature
    specifically. Cannot ignore case yet.

--- koocook_core/models/unit.py
+++ koocook_core/models/unit.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Optional
 
 import enum
 
@@ -8,31 +8,50 @@ __all__ = ['Unit', 'LengthUnit', 'AreaUnit', 'VolumeUnit', 'MassUnit',
 
 @enum.unique
 class Unit(enum.Enum):
+    def __new__(cls, symbol: Optional[str], *args):
+        obj = object.__new__(cls)
+        # ``symbol`` is canonical value
+        obj._value_ = symbol
+        return obj
+
+    def __init__(self, symbol: Optional[str], conversion_factor: Optional[float], *args):
+        cls = self.__class__
+        self._conversion_factor = conversion_factor
+        if len(args) > 1 and args[1] is not None:
+            self._singular = args[1]
+        else:
+            self._singular = ' '.join(self._name_.lower().split('_'))
+        if len(args) > 0 and args[0] is not None:
+            self._plural = args[0]
+        else:
+            self._plural = self._singular + 's'
+        for other_value in (self._singular, self._plural, *args[2:]):
+            cls._value2member_map_[other_value] = self
 
     @property
-    def symbol(self):
-        return self._value_[0]
+    def symbol(self) -> Optional[str]:
+        return self._value_
 
     @property
-    def singular(self):
-        if len(self._value_) > 3:
-            return self._value_[3]
-        return ' '.join(self._name_.lower().split('_'))
+    def singular(self) -> str:
+        return self._singular
 
     @property
-    def plural(self):
-        if len(self._value_) > 2:
-            return self._value_[2]
-        return self.singular + 's'
+    def plural(self) -> str:
+        return self._plural
 
     @property
     def conversion_factor(self) -> float:
-        return self._value_[1]
+        if self._conversion_factor is not None:
+            return self._conversion_factor
+        raise AttributeError('\'{}\' are incompatible with conversion factor'
+                             .format(self.__class__.__name__))
 
 
 class LengthUnit(Unit):
-    METRE = 'm', 1.
-    CENTIMETRE = 'cm', 0.01
+    METRE = 'm', 1., None, None, 'meter', 'meters'
+    CENTIMETRE = 'cm', 0.01, None, None, 'centimeter', 'centimeters'
+    MILLIMETRE = 'mm', 0.001, None, None, 'millimeter', 'millimeters'
     INCH = 'in', 0.0254, 'inches'
 
 
@@ -63,17 +82,16 @@ class TemperatureUnit(Unit):
     KELVIN = 'K',
     RANKINE = '°R',
 
-    @property
-    def singular(self):
-        if self is not TemperatureUnit.KELVIN:
-            return 'degree ' + self._name_.title()
-        return self._name_.lower()
-
-    @property
-    def plural(self):
-        if self is not TemperatureUnit.KELVIN:
-            return 'degrees ' + self._name_.title()
-        return self._name_.lower() + 's'
+    def __init__(self, symbol: str, *args):
+        if symbol[0] == '°':
+            _singular = 'degree ' + self._name_.title()
+            _plural = 'degrees ' + self._name_.title()
+            args = (_plural, _singular, symbol[1:])
+        else:
+            _singular = self._name_.lower()
+            _plural = self._name_.lower() + 's'
+            args = (_plural, _singular)
+        super().__init__(symbol, None, *args)
 
     @staticmethod
     def convert(value: float,
