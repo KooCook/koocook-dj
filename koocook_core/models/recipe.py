25b303234fe80d4d612559664363a3488c5b33d2
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-11-06 03:11:01 +0700

    Allor more 'Recipe' model to be null
    Forced by the datasets we have. Some fields are null but not blank to not let newly created recipes from our site have missing data.

--- koocook_core/models/recipe.py
+++ koocook_core/models/recipe.py
@@ -8,19 +8,21 @@ __all__ = ['Recipe']
 
 class Recipe(models.Model):
     name = models.CharField(max_length=63)
-    image = fields.ArrayField(models.URLField())
+    image = fields.ArrayField(models.URLField(), null=True)
     video = models.URLField(null=True, blank=True)
     author = models.ForeignKey(
         'koocook_core.Author',
         on_delete=models.PROTECT,
+        null=True,
     )
     date_published = models.DateTimeField()
-    description = models.CharField(max_length=255)
+    # description = models.CharField(max_length=255)
+    description = models.TextField()
     prep_time = models.DurationField(null=True, blank=True)
-    cook_time = models.DurationField()
+    cook_time = models.DurationField(null=True)
     # recipeingredient_set from Ingredient's ForeignKey
     recipe_instructions = fields.ArrayField(models.TextField())
-    recipe_yield = koocookfields.QuantityField(max_length=50, nau=True)
+    recipe_yield = koocookfields.QuantityField(max_length=50, nau=True, null=True)
     tag_set = models.ManyToManyField('koocook_core.Tag', blank=True)
     # comment_set from Comment's ForeignKey
     aggregate_rating = models.OneToOneField(
