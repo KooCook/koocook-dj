236ef919c29152382844a0b9720bd8dbe862b6b5
T. Bunratta <tharathorn.bu@ku.th>
2019-11-08 19:51:06 +0700

    Construct required views and support classes for the feature

--- koocook_core/views/forms/__init__.py
+++ koocook_core/views/forms/__init__.py
@@ -1,1 +1,2 @@
 from .recipe import RecipeForm
+from .comment import CommentForm
--- koocook_core/views/forms/recipe.py
+++ koocook_core/views/forms/recipe.py
@@ -5,8 +5,6 @@ from ...models import Recipe
 class RecipeForm(forms.ModelForm):
     customised_field = ['name', 'author']
 
-
-
     def __init__(self, *args, **kwargs):
         if 'user' in kwargs:
             self.user = kwargs.pop('user', None)
@@ -16,6 +14,7 @@ class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = '__all__'
+        exclude = ['author']
 
     @property
     def vanilla_fields(self):
