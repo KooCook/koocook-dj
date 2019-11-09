80cb5713e619aabbe4ec7ec1e2a102787eefa68b
T. Bunratta <tharathorn.bu@ku.th>
2019-11-09 21:09:33 +0700

    Provide serialisable models for RESTful CRUD APIs

--- koocook_core/models/post.py
+++ koocook_core/models/post.py
@@ -1,17 +1,33 @@
+import json
 from django.db import models
+from django.utils.html import mark_safe
 
-__all__ = ['Post']
+from .base import SerialisableModel
+
+__all__ = ('Post',)
 
 
-class Post(models.Model):
+class Post(SerialisableModel, models.Model):
     author = models.ForeignKey(
         'koocook_core.Author',
         on_delete=models.PROTECT,
     )
-    date_published = models.DateTimeField()
-    body = models.TextField
+    date_published = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
     # comment_set from Comment's ForeignKey
     aggregate_rating = models.OneToOneField(
-        'koocook_core.AggregateRating',
+        'koocook_core.AggregateRating', null=True,
         on_delete=models.PROTECT,
     )
+
+    # TODO: Process a Markdown body here
+    @property
+    def processed_body(self):
+        return mark_safe(self.body)
+
+    @property
+    def as_dict(self):
+        base_dict_repr = super().as_dict
+        base_dict_repr.update({'body': self.processed_body})
+        return base_dict_repr
+
