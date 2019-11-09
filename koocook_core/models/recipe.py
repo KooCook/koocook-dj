d4db39563aa8f3f73e1419a6d803dae2bd3762be
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-11-09 17:43:11 +0700

    Bring changes to models from other branches to `task/models`
    Update models to current version

--- koocook_core/models/recipe.py
+++ koocook_core/models/recipe.py
@@ -2,30 +2,36 @@ from django.contrib.postgres import fields
 from django.db import models
 
 from koocook_core import fields as koocookfields
-
+from .review import AggregateRating
 __all__ = ['Recipe']
 
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=63)
-    image = fields.ArrayField(models.URLField())
+    """
+
+    Note:
+        - description = models.CharField(max_length=255)
+        - recipeingredient_set from Ingredient's ForeignKey
+        - comment_set from Comment's ForeignKey
+    """
+    name = models.CharField(max_length=255)
+    image = fields.ArrayField(models.CharField(max_length=200), null=True)
     video = models.URLField(null=True, blank=True)
     author = models.ForeignKey(
         'koocook_core.Author',
         on_delete=models.PROTECT,
+        null=True,
     )
-    date_published = models.DateTimeField()
-    description = models.CharField(max_length=255)
+    date_published = models.DateTimeField(null=True)
+    description = models.TextField()
     prep_time = models.DurationField(null=True, blank=True)
-    cook_time = models.DurationField()
-    # ingredient_set from Ingredient's ForeignKey
+    cook_time = models.DurationField(null=True)
     recipe_instructions = fields.ArrayField(models.TextField())
-    recipe_yield = koocookfields.QuantityField(max_length=50, nau=True)
+    recipe_yield = koocookfields.QuantityField(null=True)
     tag_set = models.ManyToManyField('koocook_core.Tag', blank=True)
-    # comment_set from Comment's ForeignKey
     aggregate_rating = models.OneToOneField(
         'koocook_core.AggregateRating',
-        on_delete=models.PROTECT,
+        on_delete=models.PROTECT, blank=True, null=True, default=None
     )
 
     @property
@@ -34,10 +40,9 @@ class Recipe(models.Model):
 
     @property
     def nutrition(self):
-        pass
         return
 
     @property
-    def recipe_ingredient(self):
+    def recipe_ingredients(self):
         """ Proxy property for consistency with Schema.org's standard """
-        return self.ingredient_set
+        return self.recipeingredient_set.all()
--- koocook_core/support/quantity.py
+++ koocook_core/support/quantity.py
@@ -1,46 +1,24 @@
 from typing import Union
 
-from django.db import models, connection
-from django.core import checks
-from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
-
-from koocook_core.support.unit import *
-
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from koocook_core.support import unit as unit_
 
 __all__ = ['Quantity', 'QuantityField', 'parse_quantity']
 
 
 class Quantity:
-    __slots__ = ('amount', 'unit', 'nau')
+    __slots__ = ('amount', 'unit')
 
     def __init__(self,
                  amount: float,
-                 unit: Union[Unit, str],
-                 nau: bool = False):
-        self.nau = nau
+                 unit: Union[unit_.Unit, str]):
         if isinstance(amount, float):
             self.amount = amount
         else:
             self.amount = float(amount)
-        if nau:
-            self.unit = type('NonUnit', (), {})
-            if amount == 1:
-                self.unit.singular = unit
-            else:
-                self.unit.plural = unit
-            return
-        if isinstance(unit, Unit):
-            self.unit = unit
-        else:
-            for _unit_ in Units:
-                try:
-                    self.unit = _unit_(unit)
-                    break
-                except ValueError:
-                    pass
-            else:
-                raise ValueError('\'{}\' is not a valid Unit'.format(unit))
+        self.unit = unit_.get_unit(unit)
 
     def __str__(self):
         if self.amount == 1:
@@ -50,80 +28,44 @@ class Quantity:
         return '{} {}'.format(self.amount, self.unit.plural)
 
     def get_db_str(self):
-        return self.__str__() + ' /nau' if self.nau else ''
+        return self.__str__()
 
     @property
     def not_a_unit(self):
-        return self.nau
+        return self.unit is unit_.SpecialUnit.NONE
+
+    # Monkey patched
+    def __len__(self):
+        return len(str(self))
 
 
 def parse_quantity(quantity_string: str) -> Quantity:
-    amount, *unit, nau = quantity_string.split(' ')
-    if nau != '/nau':
-        unit = unit + [nau]
-        nau = False
+    amount, *unit = quantity_string.split(' ')
     amount = float(amount)
-    unit = ' '.join(unit)
-    if nau:
-        nau = True
     try:
-        return Quantity(amount, unit, nau)
+        return Quantity(amount, ' '.join(unit))
     except ValueError as e:
         raise ValidationError(_("Invalid input for a Quantity instance")
-                              ) from e.__context__
+                              ) from e
 
 
-class QuantityField(models.Field):
+class QuantityField(models.CharField):
     description = _("<number><space><unit> (up to %(max_length)s)")
 
-    def __init__(self, nau: bool = False, *args, **kwargs):
-        self.nau = nau
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 50
+        self.max_length = 50
         super().__init__(*args, **kwargs)
 
-    def deconstruct(self):
-        name, path, args, kwargs = super().deconstruct()
-        # Only include kwarg if it's not the default
-        if self.nau:
-            kwargs['nau'] = self.nau
-        return name, path, args, kwargs
-
-    def check(self, **kwargs):
-        return [
-            *super().check(**kwargs),
-            *self._check_max_length_attribute(**kwargs),
-        ]
-
-    def _check_max_length_attribute(self, **kwargs):
-        if self.max_length is None:
-            return [
-                checks.Error(
-                    "CharFields must define a 'max_length' attribute.",
-                    obj=self,
-                    id='fields.E120',
-                )
-            ]
-        elif (not isinstance(self.max_length, int) or isinstance(self.max_length, bool) or
-                self.max_length <= 0):
-            return [
-                checks.Error(
-                    "'max_length' must be a positive integer.",
-                    obj=self,
-                    id='fields.E121',
-                )
-            ]
-        else:
-            return []
-
-    def cast_db_type(self, connection):
-        if self.max_length is None:
-            return connection.ops.cast_char_field_without_max_length
-        return super().cast_db_type(connection)
-
-    def get_internal_type(self):
-        return 'CharField'
+    # def deconstruct(self):
+    #     name, path, args, kwargs = super().deconstruct()
+    #     # Only include kwarg if it's not the default
+    #     if self.nau:
+    #         kwargs['nau'] = self.nau
+    #     return name, path, args, kwargs
 
     def from_db_value(self, value, expression, connection):
-        if value is None:
+        if value is None or value is '':
             return value
         return parse_quantity(value)
 
@@ -143,23 +85,8 @@ class QuantityField(models.Field):
         if value is None:
             return value
 
-        if isinstance(value, str):
-            if ' /nau' in value:
-                return value
-            return value + ' /nau' if self.nau else ''
         return parse_quantity(value).get_db_str()
 
     def value_to_string(self, obj):
         value = self.value_from_object(obj)
         return self.get_prep_value(value)
-
-    def formfield(self, **kwargs):
-        # Passing max_length to forms.CharField means that the value's length
-        # will be validated twice. This is considered acceptable since we want
-        # the value in the form field (to pass into widget for example).
-        defaults = {'max_length': self.max_length}
-        # TODO: Handle multiple backends with different feature flags.
-        if self.null and not connection.features.interprets_empty_strings_as_nulls:
-            defaults['empty_value'] = None
-        defaults.update(kwargs)
-        return super().formfield(**defaults)
