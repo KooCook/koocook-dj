e669c76439c42d0cf1ea06d9e37d10f70dfa5571
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-11-18 22:34:33 +0700

    Add tests for TestAggregateRating

--- koocook_core/tests/test_models/__init__.py
+++ koocook_core/tests/test_models/__init__.py
@@ -1,28 +1,74 @@
 from django import test as djangotest
+from django.core.exceptions import ValidationError
 
 from koocook_core import models
 
 
 class TestAggregateRatingModel(djangotest.TestCase):
     def test_init_default_fields(self):
-        pass
+        aggr = models.AggregateRating()
+        self.assertEqual(aggr.best_rating, 5)
+        self.assertEqual(aggr.worst_rating, 1)
 
     def test_create_empty(self):
-        pass
-
-    def test_add_rating(self):
-        pass
-
-    def test_remove_rating(self):
-        pass
-
-    def test_check_rating(self):
-        pass
+        aggr = models.AggregateRating.create_empty()
+        self.assertIsInstance(aggr, models.AggregateRating)
+        self.assertEqual(aggr.best_rating, 5)
+        self.assertEqual(aggr.worst_rating, 1)
+        self.assertEqual(aggr.rating_value, 0)
+        self.assertEqual(aggr.rating_count, 0)
 
     def test_item_reviewed_getter(self):
-        pass
+        with self.subTest(item=models.Post.__qualname__):
+            item = models.Post()
+            self.assertIs(item.aggregate_rating.post, item)
+            self.assertIs(item.aggregate_rating.item_reviewed, item)
 
-    def test_item_reviewed_setter(self):
+        with self.subTest(item=models.Recipe.__qualname__):
+            item = models.Recipe()
+            self.assertIs(item.aggregate_rating.recipe, item)
+            self.assertIs(item.aggregate_rating.item_reviewed, item)
+
+        with self.subTest(item=models.Comment.__qualname__):
+            item = models.Comment()
+            self.assertIs(item.aggregate_rating.comment, item)
+            self.assertIs(item.aggregate_rating.item_reviewed, item)
+
+    def test_check_rating(self):
+        author = models.Author(name='author name')
+        rating = models.Rating(author=author, rating_value=5)
+        for cls in (models.Post, models.Recipe, models.Comment):
+            with self.subTest(item=cls.__qualname__):
+                item = cls()
+                rating.item_reviewed = item
+                try:
+                    item.aggregate_rating.check_rating(rating)
+                except Exception as e:
+                    raise self.failureException('unexpected exception raised') from e
+
+    def test_add_rating(self):
+        author = models.Author(name='author name')
+        for cls in (models.Post, models.Recipe, models.Comment):
+            item = cls()
+            # TODO: Add function to initialize Recipe and Post so that it is savable
+            rating = models.Rating(author=author, rating_value=5)
+            rating.item_reviewed = item
+            with self.subTest(subtest='from empty', item=cls.__qualname__):
+                item.aggregate_rating.add_rating(rating)
+                self.assertEqual(item.aggregate_rating.rating_count, 1)
+                self.assertEqual(item.aggregate_rating.rating_value, 5)
+            rating = models.Rating(author=author, rating_value=3)
+            rating.item_reviewed = item
+            with self.subTest(subtest='add second', item=cls.__qualname__):
+                item.aggregate_rating.add_rating(rating)
+                self.assertEqual(item.aggregate_rating.rating_count, 2)
+                self.assertEqual(item.aggregate_rating.rating_value, 4)
+            # Re-using ratings should result in error
+            with self.subTest(subtest='re-add rating', item=cls.__qualname__):
+                with self.assertRaises(ValidationError):
+                    item.aggregate_rating.add_rating(rating)
+
+    def test_remove_rating(self):
         pass
 
 
