56098de83ef34f80dece46e7d0b01cb917f4e1b7
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-11-18 22:37:15 +0700

    Refactor models/review.py so that (most) of the tests pass

--- koocook_core/models/review.py
+++ koocook_core/models/review.py
@@ -1,8 +1,111 @@
+from typing import Union
+from decimal import Decimal
+
 from django.db import models
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.utils.translation import gettext_lazy as _
 
-__all__ = ['Comment', 'Rating', 'AggregateRating']
+__all__ = ['AggregateRating', 'Rating', 'Comment']
+
+
+def _add_rating(old_value: Decimal, old_count: int,
+                new_rating: Union[int, float]) -> Decimal:
+    old_total = old_value * old_count
+    return (old_total + new_rating) / (old_count + 1)
+
+
+def _remove_rating(old_value: Decimal, old_count: int,
+                   new_rating: Union[int, float]) -> Decimal:
+    old_total = old_value * old_count
+    return (old_total - new_rating) / (old_count - 1)
+
+
+class AggregateRating(models.Model):
+    """
+
+    Notes:
+        attribute 'recipe' from Recipe's OneToOneField
+        attribute 'post' from Post's OneToOneField
+        attribute 'comment' from Comment's OneToOneField
+    """
+    rating_value = models.DecimalField(
+        decimal_places=10,
+        max_digits=13,
+        # every vote counts until 10 B
+    )
+    rating_count = models.IntegerField()
+    best_rating = models.IntegerField(default=5)
+    worst_rating = models.IntegerField(default=1)
+
+    def check_rating(self, rating: 'Rating') -> None:
+        """Checks if rating is of the same type and origin or not.
+
+        Raises:
+            ValidationError: When `rating` is not valid
+        """
+        if rating.item_reviewed != self.item_reviewed:
+            raise ValidationError(_("incompatible item_reviewed: '{}' != '{}'"
+                                    .format(rating.item_reviewed, self.item_reviewed)))
+        if rating.best_rating != self.best_rating:
+            raise ValidationError(_("incompatible best_rating: '{}' != '{}'"
+                                    .format(rating.best_rating, self.best_rating)))
+        if rating.worst_rating != self.worst_rating:
+            raise ValidationError(_("incompatible worst_rating: '{}' != '{}'"
+                                    .format(rating.worst_rating, self.worst_rating)))
+
+    def add_rating(self, rating: 'Rating'):
+        """Adds a rating from an aggregate rating.
+
+        Raises:
+            ValidationError: When `rating` is not valid
+        """
+        self.check_rating(rating)
+        if rating.used:
+            raise ValidationError(_("rating is already tabulated somewhere, can't add"))
+        self.rating_value = _add_rating(self.rating_value, self.rating_count, rating.rating_value)
+        self.rating_count += 1
+        self.save()
+        assert not rating.used, "rating shouldn't have been used yet"
+        rating.used = True
+        rating.save()
+
+    def remove_rating(self, rating: 'Rating'):
+        """Removes a rating from an aggregate rating.
+
+        Raises:
+            ValidationError: When `rating` is not valid
+        """
+        self.check_rating(rating)
+        if not rating.used:
+            raise ValidationError(_("rating is not tabulated yet, can't remove"))
+        self.rating_value = _remove_rating(self.rating_value, self.rating_count, rating.rating_value)
+        self.rating_count -= 1
+        self.save()
+        assert rating.used, "rating should have been used"
+        rating.used = False
+        rating.save()
+
+    @property
+    def item_reviewed(self) -> Union['Recipe', 'Post', 'Comment', None]:
+        try:
+            return self.recipe
+        except ObjectDoesNotExist:
+            pass
+        try:
+            return self.post
+        except ObjectDoesNotExist:
+            pass
+        try:
+            return self.comment
+        except ObjectDoesNotExist:
+            pass
+
+    @classmethod
+    def create_empty(cls, **kwargs) -> 'AggregateRating':
+        """Creates a new empty ``aggregate rating``."""
+        kwargs['rating_value'] = kwargs.pop('rating_value', 0)
+        kwargs['rating_count'] = kwargs.pop('rating_count', 0)
+        return cls.objects.create(**kwargs)
 
 
 class Comment(models.Model):
@@ -15,8 +118,8 @@ class Comment(models.Model):
     aggregate_rating = models.OneToOneField(
         'koocook_core.AggregateRating',
         on_delete=models.PROTECT,
+        default=AggregateRating.create_empty,
     )
-    # item_reviewed = models.URLField()
     reviewed_recipe = models.ForeignKey(
         'koocook_core.Recipe',
         on_delete=models.CASCADE,
@@ -37,28 +140,24 @@ class Comment(models.Model):
     )
 
     @property
-    def item_reviewed(self):
+    def item_reviewed(self) -> Union['Recipe', 'Post', 'Comment', None]:
         return self.reviewed_recipe or self.reviewed_post or self.reviewed_comment
 
     @item_reviewed.setter
-    def item_reviewed(self, obj):
+    def item_reviewed(self, obj: Union['Recipe', 'Post', 'Comment']):
         from koocook_core.models.recipe import Recipe
         from koocook_core.models.post import Post
-        try:
-            if self.reviewed_recipe:
-                assert isinstance(obj, Recipe)
-                self.reviewed_recipe = obj
-            elif self.reviewed_post:
-                assert isinstance(obj, Post)
-                self.reviewed_post = obj
-            elif self.reviewed_comment:
-                assert isinstance(obj, self.__class__)
-                self.reviewed_comment = obj
-        except AssertionError as e:
+
+        if isinstance(obj, Recipe):
+            self.reviewed_recipe = obj
+        elif isinstance(obj, Post):
+            self.reviewed_post = obj
+        elif isinstance(obj, Comment):
+            self.reviewed_comment = obj
+        else:
             raise TypeError('item_reviewed must be of the correct type '
-                            '\'{}\' not \'\''.format(
-                             type(self.reviewed_recipe or self.reviewed_post or self.reviewed_comment),
-                             type(obj))) from e.__context__
+                            "'{}' not '{}'"
+                            .format(type(self.item_reviewed), type(obj)))
 
 
 class Rating(models.Model):
@@ -69,7 +168,6 @@ class Rating(models.Model):
     rating_value = models.IntegerField()
     best_rating = models.IntegerField(default=5)
     worst_rating = models.IntegerField(default=1)
-    # item_reviewed = models.URLField()
     reviewed_recipe = models.ForeignKey(
         'koocook_core.Recipe',
         on_delete=models.CASCADE,
@@ -88,94 +186,24 @@ class Rating(models.Model):
         null=True,
         blank=True,
     )
+    used = models.BooleanField(default=False, blank=True)
 
     @property
-    def item_reviewed(self):
+    def item_reviewed(self) -> Union['Recipe', 'Post', 'Comment', None]:
         return self.reviewed_recipe or self.reviewed_post or self.reviewed_comment
 
     @item_reviewed.setter
-    def item_reviewed(self, obj):
+    def item_reviewed(self, obj: Union['Recipe', 'Post', 'Comment']):
         from koocook_core.models.recipe import Recipe
         from koocook_core.models.post import Post
-        try:
-            if self.reviewed_recipe:
-                assert isinstance(obj, Recipe)
-                self.reviewed_recipe = obj
-            elif self.reviewed_post:
-                assert isinstance(obj, Post)
-                self.reviewed_post = obj
-            elif self.reviewed_comment:
-                assert isinstance(obj, Comment)
-                self.reviewed_comment = obj
-        except AssertionError as e:
+
+        if isinstance(obj, Recipe):
+            self.reviewed_recipe = obj
+        elif isinstance(obj, Post):
+            self.reviewed_post = obj
+        elif isinstance(obj, Comment):
+            self.reviewed_comment = obj
+        else:
             raise TypeError('item_reviewed must be of the correct type '
-                            '\'{}\' not \'\''.format(
-                             type(self.reviewed_recipe or self.reviewed_post or self.reviewed_comment),
-                             type(obj))) from e.__context__
-
-
-class AggregateRating(models.Model):
-    rating_value = models.DecimalField(
-        decimal_places=10,
-        max_digits=13,
-        # every vote counts until 10 B
-    )
-    rating_count = models.IntegerField()
-    best_rating = models.IntegerField(default=5)
-    worst_rating = models.IntegerField(default=1)
-    # recipe from Recipe's OneToOneField
-    # post from Post's OneToOneField
-    # comment from Comment's OneToOneField
-
-    def check_rating(self, rating: Rating) -> None:
-        """Checks if rating is of the same type and origin or not.
-
-        Raises:
-            ValidationError: When `rating` is not valid
-        """
-        if rating.best_rating != self.best_rating:
-            raise ValidationError(_('Incompatible bestRating: \'{}\' != \'\''
-                                    .format(rating.best_rating, self.best_rating)))
-        if rating.worst_rating != self.worst_rating:
-            raise ValidationError(_('Incompatible worstRating: \'{}\' != \'\''
-                                    .format(rating.worst_rating, self.worst_rating)))
-        if rating.item_reviewed != self.item_reviewed:
-            raise ValidationError(_('Incompatible itemReviewed: \'{}\' != \'\''
-                                    .format(rating.item_reviewed, self.item_reviewed)))
-
-    def add_rating(self, rating: Rating):
-        """Adds a rating from an aggregate rating.
-
-        Raises:
-            ValidationError: When `rating` is not valid
-        """
-        self.check_rating(rating)
-        total_value = self.rating_value * self.rating_count
-        total_value += rating.rating_value
-        self.rating_count += 1
-        self.rating_value = total_value / self.rating_count
-        self.save()
-
-    def remove_rating(self, rating: Rating):
-        """Removes a rating from an aggregate rating.
-
-        Raises:
-            ValidationError: When `rating` is not valid
-        """
-        self.check_rating(rating)
-        total_value = self.rating_value * self.rating_count
-        total_value -= rating.rating_value
-        self.rating_count -= 1
-        self.rating_value = total_value / self.rating_count
-        self.save()
-
-    @property
-    def item_reviewed(self):
-        return self.recipe or self.post or self.comment
-
-    @classmethod
-    def create_empty(cls, **kwargs) -> 'AggregateRating':
-        """Creates a new empty ``aggregate rating``."""
-        kwargs['rating_value'] = kwargs.pop('rating_value', 0)
-        kwargs['rating_count'] = kwargs.pop('rating_count', 0)
-        return cls.objects.create(**kwargs)
+                            "'{}' not '{}'"
+                            .format(type(self.item_reviewed), type(obj)))
