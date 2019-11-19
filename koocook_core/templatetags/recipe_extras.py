577e4b0226f0d8d0bd5d408b6fc55d3f331fb9d4
plumest <chayathon.k@ku.th>
2019-11-19 22:43:05 +0700

    ordering featured recipes with pubish date and rating

--- koocook_core/templatetags/recipe_extras.py
+++ koocook_core/templatetags/recipe_extras.py
@@ -5,4 +5,4 @@ register = template.Library()
 
 @register.filter(name='top_latest')
 def top_latest_recipes(recipes):
-    return recipes.order_by('date_published')[:10]
+    return recipes.order_by('-date_published', 'aggregate_rating')[:10]
