0eb0bbc70b7b0d22d098c33d24e06b434827e553
T. Bunratta <tharathorn.bu@ku.th>
2019-11-19 21:24:18 +0700

    Add forms for preference setting

--- koocook_core/views/forms/recipe.py
+++ koocook_core/views/forms/recipe.py
@@ -1,20 +1,12 @@
 from django import forms
+from ...support import CustomisableForm
+
 from ...models import Recipe
 
 
-class RecipeForm(forms.ModelForm):
-    customised_field = ['name', 'author']
-
-    def __init__(self, *args, **kwargs):
-        if 'user' in kwargs:
-            self.user = kwargs.pop('user', None)
-        super().__init__(*args, **kwargs)
-        # self.fields['author'].disabled = True
+class RecipeForm(CustomisableForm):
+    customised_field = ('name', 'author')
 
     class Meta:
         model = Recipe
         fields = '__all__'
-
-    @property
-    def vanilla_fields(self):
-        return [field for field in self if not (field.name in self.customised_field or field.is_hidden)]
--- koocook_core/views/forms/user.py
+++ koocook_core/views/forms/user.py
@@ -1,29 +1,29 @@
-from django import forms
 from django.contrib.auth.models import User
+from django.forms.widgets import Textarea, HiddenInput
+
 from ...models import KoocookUser
+from ...support import CustomisableForm
 
 
-class BasicProfileForm(forms.ModelForm):
+class BasicProfileForm(CustomisableForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name']
+        fields = ('first_name', 'last_name', 'email')
 
     def __init__(self, *args, **kwargs):
         if 'user' in kwargs:
             self.user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
 
-    @property
-    def vanilla_fields(self):
-        return [field for field in self if not (field.name in self.customised_field or field.is_hidden)]
 
-
-class ExtendedProfileForm(forms.ModelForm):
+class ExtendedProfileForm(CustomisableForm):
     class Meta:
         model = KoocookUser
-        fields = ['preferences', 'user_settings']
+        fields = ['preferences']
 
-    customised_field = ['user']
+        widgets = {'preferences': HiddenInput(attrs={'v-model': 'submission'})}
+
+    customised_field = ('user',)
 
     def __init__(self, *args, **kwargs):
         if 'user' in kwargs:
