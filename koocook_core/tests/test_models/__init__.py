3d09f89153cdd338124c1eadffe147a3bd26df80
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-11-20 09:37:56 +0700

    Refactor old tests with knowledge from new tests

--- koocook_core/tests/test_models/__init__.py
+++ koocook_core/tests/test_models/__init__.py
@@ -11,6 +11,33 @@ from koocook_core.tests import utils
 
 
 class TestAggregateRatingModel(djangotest.TestCase):
+
+    def setUp(self) -> None:
+        author = Author.objects.create(name='')
+        user = User.objects.create(email='koocook@gmail.com')
+        self.test_authors = [author, user.koocookuser.author]
+        self.test_objects = []
+
+        for author in self.test_authors:
+            recipe = Recipe.objects.create(author=author,
+                                           name='recipe name',
+                                           recipe_instructions=[])
+            post = Post.objects.create(author=author)
+            self.test_objects.extend([recipe, post])
+
+        for author in self.test_authors:
+            for obj in self.test_objects.copy():
+                comment = Comment.objects.create(author=author, item_reviewed=obj)
+                self.test_objects.append(comment)
+
+    def clean_up_aggregate_rating(self):
+        """Resets the state of aggregate ratings, for testing only."""
+        for item in self.test_objects:
+            aggr = item.aggregate_rating
+            aggr.rating_value = 0
+            aggr.rating_count = 0
+            aggr.save()
+
     def test_field_settings(self):
         aggr = AggregateRating()
         with self.subTest(field='rating_value'):
@@ -32,58 +59,56 @@ class TestAggregateRatingModel(djangotest.TestCase):
         with self.subTest('type'):
             self.assertIsInstance(aggr, AggregateRating)
         for attr, value in (
-            ('best_rating', 5),
-            ('worst_rating', 1),
-            ('rating_value', 0),
-            ('rating_count', 0),
+                ('best_rating', 5),
+                ('worst_rating', 1),
+                ('rating_value', 0),
+                ('rating_count', 0),
         ):
             with self.subTest('values', attr=attr):
                 self.assertEqual(getattr(aggr, attr), value)
 
     def test_item_reviewed_getter(self):
-        for cls in (Post, Recipe, Comment):
-            with self.subTest(item=cls.__qualname__):
-                item = cls()
+        for item in self.test_objects:
+            with self.subTest(item=item.__class__.__qualname__):
                 self.assertIs(
-                    getattr(item.aggregate_rating, cls.__name__.lower()), item)
+                    getattr(item.aggregate_rating,
+                            item.__class__.__name__.lower()), item)
                 self.assertIs(item.aggregate_rating.item_reviewed, item)
 
     def test_check_rating(self):
-        author = Author(name='author name')
-        for cls in (Post, Recipe, Comment):
-            with self.subTest(item=cls.__qualname__):
-                item = cls()
-                rating = Rating(author=author, rating_value=5)
-                rating.item_reviewed = item
-                try:
-                    item.aggregate_rating.check_rating(rating)
-                except Exception as e:
-                    raise self.failureException(
-                        'unexpected exception raised') from e
+        for author in self.test_authors:
+            for item in self.test_objects:
+                with self.subTest(author=author, item=item.__class__.__qualname__):
+                    rating = Rating(author=author, rating_value=5)
+                    rating.item_reviewed = item
+                    try:
+                        item.aggregate_rating.check_rating(rating)
+                    except Exception as e:
+                        raise self.failureException(
+                            'unexpected exception raised') from e
 
     def test_add_rating(self):
-        author = Author(name='author name')
-        for cls in (Post, Recipe, Comment):
-            item = cls()
-            # TODO: Add function to initialize Recipe and Post so that it is savable
-            rating = Rating(author=author, rating_value=5)
-            rating.item_reviewed = item
-            with self.subTest('from empty', item=cls.__qualname__):
-                item.aggregate_rating.add_rating(rating)
-                self.assertEqual(item.aggregate_rating.rating_count, 1)
-                self.assertEqual(item.aggregate_rating.rating_value, 5)
-
-            rating = Rating(author=author, rating_value=3)
-            rating.item_reviewed = item
-            with self.subTest('add second', item=cls.__qualname__):
-                item.aggregate_rating.add_rating(rating)
-                self.assertEqual(item.aggregate_rating.rating_count, 2)
-                self.assertEqual(item.aggregate_rating.rating_value, 4)
-
-            # Re-using ratings should result in error
-            with self.subTest('re-add rating', item=cls.__qualname__):
-                with self.assertRaises(ValidationError):
+        for author in self.test_authors:
+            for item in self.test_objects:
+                rating = Rating(author=author, rating_value=5)
+                rating.item_reviewed = item
+                with self.subTest('from empty', author=author, item=item.__class__.__qualname__):
                     item.aggregate_rating.add_rating(rating)
+                    self.assertEqual(item.aggregate_rating.rating_count, 1)
+                    self.assertEqual(item.aggregate_rating.rating_value, 5)
+
+                rating = Rating(author=author, rating_value=3)
+                rating.item_reviewed = item
+                with self.subTest('add second', author=author, item=item.__class__.__qualname__):
+                    item.aggregate_rating.add_rating(rating)
+                    self.assertEqual(item.aggregate_rating.rating_count, 2)
+                    self.assertEqual(item.aggregate_rating.rating_value, 4)
+
+                # Re-using ratings should result in error
+                with self.subTest('re-add rating', author=author, item=item.__class__.__qualname__):
+                    with self.assertRaises(ValidationError):
+                        item.aggregate_rating.add_rating(rating)
+            self.clean_up_aggregate_rating()
 
     def test_remove_rating(self):
         pass
@@ -93,18 +118,18 @@ class TestAggregateRatingModel(djangotest.TestCase):
         # add more as necessary
         for i in (0, 0.):
             with self.subTest('test manual', i=i, old=(0, 0)):
-                self.assertEqual(review._add_rating(Decimal(0), 0, i), i)
+                self.assertEqual(models_.review._add_rating(Decimal(0), 0, i), i)
 
         # do monkey test, 999 is the maximum rating_value set
         for i in utils.gen_ints(-1000, 1000, 100):
             with self.subTest('test int', i=i, old=(0, 0)):
-                self.assertEqual(review._add_rating(Decimal(0), 0, i), i)
+                self.assertEqual(models_.review._add_rating(Decimal(0), 0, i), i)
 
         for i in utils.gen_floats(-1000, 1000, 1_000):
             i = round(i, 1)
             with self.subTest('test float', i=i, old=(0, 0)):
                 # current use is with 1 decimal places for new ratings
-                self.assertEqual(review._add_rating(Decimal(0), 0, i),
+                self.assertEqual(models_.review._add_rating(Decimal(0), 0, i),
                                  round(Decimal(i), 1))
 
     # TODO: Add these tests
@@ -136,10 +161,10 @@ class TestAuthorModel(djangotest.TestCase):
 
     def test_str(self):
         for expected_str, user in (
-            ('Alice Merryweather', self.test_user),
-            ('Bob', User.objects.create(first_name='Bob', username='Bobby')),
-            ('C123', User.objects.create(username='C123')),
-            ('Dylan', User.objects.create(last_name='Dylan', username='DD')),
+                ('Alice Merryweather', self.test_user),
+                ('Bob', User.objects.create(first_name='Bob', username='Bobby')),
+                ('C123', User.objects.create(username='C123')),
+                ('Dylan', User.objects.create(last_name='Dylan', username='DD')),
         ):
             with self.subTest('author with user',
                               first_name=user.first_name,
@@ -320,8 +345,8 @@ class TestKoocookUserModel(djangotest.TestCase):
 class TestQuantityField(djangotest.TestCase):
     def test_quantity_field_max_length(self):
         for model, field in (
-            ('RecipeIngredient', 'quantity'),
-            ('Recipe', 'recipe_yield'),
+                ('RecipeIngredient', 'quantity'),
+                ('Recipe', 'recipe_yield'),
         ):
             with self.subTest(model=model, field=field):
                 m = getattr(models_, model)()
