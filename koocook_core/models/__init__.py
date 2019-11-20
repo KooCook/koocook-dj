40604c4c6166923477fc14179c91fb2c9d130a6e
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-11-20 09:40:50 +0700

    Add `__all__` to models/ and make it dynamic

--- koocook_core/models/__init__.py
+++ koocook_core/models/__init__.py
@@ -1,6 +1,12 @@
-from .nutrition import RecipeIngredient, MetaIngredient
+import itertools
+
+__all__ = []
+
+from .nutrition import MetaIngredient, RecipeIngredient
 from .post import Post
 from .recipe import Recipe
 from .review import AggregateRating, Comment, Rating
 from .tag import Tag, TagLabel
 from .user import Author, KoocookUser
+
+__all__ = list(itertools.chain.from_iterable(_.__all__ for _ in (nutrition, post, recipe, review, tag, user)))
