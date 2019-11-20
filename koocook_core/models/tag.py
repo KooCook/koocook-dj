f4273fb52eb2161af87967f97516581d772d7823
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-11-20 17:01:36 +0700

    Add docstrings to models 'Tag' and 'TagLabel'

--- koocook_core/models/tag.py
+++ koocook_core/models/tag.py
@@ -4,11 +4,30 @@ __all__ = ['Tag', 'TagLabel']
 
 
 class Tag(models.Model):
+    """Represents a ``tag`` of recipes.
+
+    Some examples of valid use cases:
+        <Tag: French, <Label: cuisine>>,
+        <Tag: gluten-free, <Label: clearance>>,
+        <Tag: seafood, <Label: allergen>>,
+
+    Attributes:
+        name (str): name of the tag
+        recipe_set (RelatedManager): from ManyToMany in ``Recipe``
+    """
     name = models.CharField(max_length=50)
     label = models.ForeignKey('koocook_core.TagLabel', null=True, blank=True, on_delete=models.SET_NULL)
-    # recipe_set from Recipe's ManyToMany
 
 
 class TagLabel(models.Model):
+    """Represents a ``label`` (category) of tags.
+
+    Some examples of valid use cases:
+        <Label: allergen>, <label: diet>, <Label: warning>,
+        <Label: clearance>, <Label: celebration>, <Label: cuisine>,
+
+    Attributes:
+        name (str): name of the label
+        tag_set (RelatedManager): from ForeignKey in ``Tag``
+    """
     name = models.CharField(max_length=50)
-    # tag_set from Tag's ForeignKey
