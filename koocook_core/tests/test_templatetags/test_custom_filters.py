a1d2d5fb066dfeb4b2f2eb69e1e7224bea37bb7c
plumest <chayathon.k@ku.th>
2019-11-21 10:00:54 +0700

    Add test for tag level

--- koocook_core/tests/test_templatetags/test_custom_filters.py
+++ koocook_core/tests/test_templatetags/test_custom_filters.py
@@ -8,6 +8,10 @@ class CustomTagTests(TestCase):
         tag_label = create_tag_label(name='label 1', level=4)
         self.assertEqual(tag_level(tag_label.level), 'is-danger')
 
+    def test_tag_level_with_over_level(self):
+        tag_label = create_tag_label(name='label 1', level=100)
+        self.assertIsNone(tag_level(tag_label.level))
+
 
 def create_tag_label(name: str, level: int = 1):
     return TagLabel.objects.create(name=name, level=level)
