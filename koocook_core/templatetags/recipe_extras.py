931e893aa0cd851ae2e8bbe7c50dbca470302636
plumest <chayathon.k@ku.th>
2019-11-21 09:55:01 +0700

    Using meaningful variable

--- koocook_core/templatetags/recipe_extras.py
+++ koocook_core/templatetags/recipe_extras.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 
 @register.filter(name='tag_level')
-def tag_level(lv: int):
+def tag_level(lv: int) -> str:
     """ Return Bulma color helper by given label_level """
     level = {1: 'is-white', 2: 'is-success', 3: 'is-warning', 4: 'is-danger'}
     return level.get(lv)
