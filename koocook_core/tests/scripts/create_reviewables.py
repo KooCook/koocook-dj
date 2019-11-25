72067bca0555015fac5d9c913df275449f7a91ca
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-11-25 19:42:33 +0700

    Reduce test reviewable instances

--- koocook_core/tests/scripts/create_reviewables.py
+++ koocook_core/tests/scripts/create_reviewables.py
@@ -3,10 +3,10 @@ from koocook_core.models import Author, Recipe, Post, Comment
 
 def main():
     for i, author in enumerate(Author.objects.all()):
-        m = i % 4
-        if m % 2 == 0:
+        m = i % 3
+        if m == 0:
             Recipe.objects.create(author=author, name=f'recipe {i}')
-        if m % 2 == 1:
+        if m == 1:
             Post.objects.create(author=author)
     authors = Author.objects.all()
     for i, obj in enumerate(list(Recipe.objects.all()) + list(Post.objects.all())):
