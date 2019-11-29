4d5629c1b59e4e452d38b9dd52f0585069123101
T. Bunratta <tharathorn.bu@ku.th>
2019-11-29 20:53:04 +0700

    More unit tests and fixes for Reviewables and their Controllers

--- koocook_core/tests/base.py
+++ koocook_core/tests/base.py
@@ -2,7 +2,7 @@ from typing import Dict, Any
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from ..models import Author, Post
+from ..models import Author, KoocookUser, Post, Recipe
 
 
 def create_dummy_post(user: User) -> Post:
@@ -17,6 +17,13 @@ def create_dummy_comment_dict() -> Dict[str, Any]:
     return comment_fields
 
 
+def create_dummy_recipe(author: Author) -> Recipe:
+    recipe = Recipe(author=author, name="Dummy recipe")
+    recipe.recipe_instructions = "{}"
+    recipe.save()
+    return recipe
+
+
 class AuthTestCase(TestCase):
     def setUp(self) -> None:
         self.username = "testuser"
@@ -25,4 +32,5 @@ class AuthTestCase(TestCase):
         self.user = User.objects.create_user(self.username, password=password)
         self.user2 = User.objects.create_user("testuser2", password=self.password)
         self.client.login(username=self.username, password=password)
+        self.kc_user = KoocookUser.objects.get(user=self.user)
         self.author = Author.objects.get(user__user=self.user)
