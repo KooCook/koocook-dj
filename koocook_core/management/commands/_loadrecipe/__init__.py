c507837a570341de4bb952a2dc527dc24846e560
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-11-30 17:50:10 +0700

    Change structure to avoid `print` in Django commands

--- koocook_core/management/commands/_loadrecipe/__init__.py
+++ koocook_core/management/commands/_loadrecipe/__init__.py
@@ -5,7 +5,6 @@ class Command(BaseCommand):
     help = 'load recipe data'
 
     def handle(self, *args, **options):
-        self.stdout.write('I was called')
         self.stdout.write('calling read_data')
         from .._add_path import add_datatrans
         add_datatrans()
