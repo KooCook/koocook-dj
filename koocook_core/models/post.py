2cc622c285ab9ed6b68eb0718204cd5243fe1860
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-11-30 17:44:15 +0700

    Fix merge error: AggregateRating not imported

--- koocook_core/models/post.py
+++ koocook_core/models/post.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils.html import mark_safe
 
 from .base import SerialisableModel
-from .review import create_empty_aggregate_rating
+from .review import AggregateRating
 from ..support import FormattedField
 
 __all__ = ('Post',)
