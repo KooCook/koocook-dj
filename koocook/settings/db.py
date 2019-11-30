f00383e65207aaa82af86b9bab86171eb2a2c1e5
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-11-30 18:49:15 +0700

    Fix imports unused cause by import from other imported modules

--- koocook/settings/db.py
+++ koocook/settings/db.py
@@ -1,20 +1,20 @@
-from decouple import config
-from .dirs import BASE_DIR
+from decouple import config as _config
+from .dirs import BASE_DIR as _BASE_DIR
 
-DATABASE_NAME = config('DATABASE_NAME', (BASE_DIR / 'db.sqlite3').as_posix())
+DATABASE_NAME = _config('DATABASE_NAME', (_BASE_DIR / 'db.sqlite3').as_posix())
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': config('DATABASE_ENGINE', 'django.db.backends.sqlite3'),
+        'ENGINE': _config('DATABASE_ENGINE', 'django.db.backends.sqlite3'),
         'NAME': DATABASE_NAME,
-        'USER': config('DATABASE_USERNAME', 'username'),
-        'PASSWORD': config('DATABASE_PASSWORD', 'password'),
-        'HOST': config('DATABASE_HOST', '127.0.0.1'),
-        'PORT': config('DATABASE_PORT', '5432'),
+        'USER': _config('DATABASE_USERNAME', 'username'),
+        'PASSWORD': _config('DATABASE_PASSWORD', 'password'),
+        'HOST': _config('DATABASE_HOST', '127.0.0.1'),
+        'PORT': _config('DATABASE_PORT', '5432'),
         'TEST': {
-            'NAME': config('TEST_DATABASE_NAME', 'test_' + DATABASE_NAME),
+            'NAME': _config('TEST_DATABASE_NAME', 'test_' + DATABASE_NAME),
         }
     }
 }
