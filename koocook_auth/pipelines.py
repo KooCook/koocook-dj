d2f60f423cd1940915a80e417e832eddf89050cf
T. Bunratta <tharathorn.bu@ku.th>
2019-12-13 17:01:27 +0700

    Append the avatar association logic

--- koocook_auth/pipelines.py
+++ koocook_auth/pipelines.py
@@ -1,15 +1,17 @@
 from social_core.backends.google import GoogleOAuth2
+import logging
+import urllib.request
+
+logger = logging.getLogger(__name__)
 
 
 def associate_social_profile(backend, user, response, *args, **kwargs):
     if isinstance(backend, GoogleOAuth2):
-        print(response.get('picture'))
-        if response.get('image') and response['image'].get('url'):
-            url = response['image'].get('url')
-            ext = url.split('.')[-1]
-            user.avatar.save(
-               '{0}.{1}'.format('avatar', ext),
-               ContentFile(urllib2.urlopen(url).read()),
-               save=False
-            )
-            user.save()
+        logger.info(f"Associate social profile with {user.username}")
+        if response.get('picture'):
+            url = response.get('picture')
+            req = urllib.request.Request(url=url)
+            resp = urllib.request.urlopen(req)
+            if resp.status not in [400, 404, 403, 408, 409, 501, 502, 503]:
+                user.koocook_user.avatar = {'type': 'url', 'content': url}
+                user.koocook_user.save()
